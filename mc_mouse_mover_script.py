344d79be987b21fcf228f26482759d57c7fa4e56
Dmitry Kartashov <57914020+loneymist@users.noreply.github.com>
2022-10-20 20:16:06 +0700

    b0.1

---
+++ mc_mouse_mover_script.py
@@ -0,0 +1,11 @@
+import mouse 
+import keyboard
+import time
+
+time.sleep(5)
+
+while True:
+    mouse.move(100,5, absolute=False, duration=0.5)
+    mouse.move(-100,-5, absolute=False, duration=0.5)
+##(707, 669)
+##(1095, 750)
