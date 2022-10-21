664c463cec0573a5a3fb985f486926128bbe679d
Dmitry Kartashov <57914020+loneymist@users.noreply.github.com>
2022-10-21 16:52:40 +0700

    semi-ready

--- mc_mouse_mover_script.py
+++ mc_mouse_mover_script.py
@@ -4,14 +4,18 @@ import keyboard
 import time
 c=0
 while True:
-    if keyboard.is_pressed("z"):
+    if keyboard.is_pressed("="):
         c=1
     while c == 1:
             keyboard.press("w")
+            time.sleep(0.15)
+            keyboard.release("w")
+            pyautogui.moveTo(960, 540,duration=0.2)
             keyboard.press("s")
-            pyautogui.moveTo(990, 540,duration=1)
-            pyautogui.moveTo(930, 540,duration=1)
-            keyboard.press("w")
-            keyboard.press("s")
+            time.sleep(0.3)
+            keyboard.release("s")
+            pyautogui.moveTo(990, 540,duration=0.2)
+            pyautogui.moveTo(930, 540,duration=0.2)
+            pyautogui.moveTo(960, 540,duration=0.2)
             if keyboard.is_pressed("x")==True:
                 c=0
